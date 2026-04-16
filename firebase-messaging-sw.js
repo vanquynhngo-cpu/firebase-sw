f8e1aaf2b823c5a004a6f22f187e3e67dc0bd0c6
vanquynhngo-cpu <vanquynh.ngo@spxexpress.com>
2026-04-16 12:38:45 +0700

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -57,7 +57,7 @@ self.addEventListener('notificationclick', (event) => {
     const responseValue = (action === 'yes') ? 'Tham gia' : 'Không tham gia';
     
     // URL API của Google Apps Script của bạn
-    const gasApiUrl = "https://script.google.com/macros/s/AKfycbyiFxIzwImjYgQh85cdR2BdElDozHFFISIbI6GEXmksDPz9gpLPAS9MZnhhKa_8FEqyXQ/exec";
+    const gasApiUrl = "https://script.google.com/macros/s/AKfycbz8a3j6JflOMrimVP0MKwE30KETWWj1JnORYyNFqwOWyS_ZQIK9isHHwx1kf0p2blB5Hg/exec";
 
     event.waitUntil(
       fetch(gasApiUrl, {
